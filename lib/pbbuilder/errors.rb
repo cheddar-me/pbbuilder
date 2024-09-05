ec245cda69ce296d7b25b0b5012fafdacc289d4d
Sebastian van Hesteren <sebastian@cheddar.me>
2024-09-05 14:11:06 +0200

    Remove explicit Object/BasicObject inheritence
    in a seperate file.

--- lib/pbbuilder/errors.rb
+++ lib/pbbuilder/errors.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "pbbuilder/pbbuilder"
-
 class Pbbuilder
   class MergeError < ::StandardError
     def self.build(current_value, updates)
--- lib/pbbuilder/pbbuilder.rb
+++
@@ -1,5 +0,0 @@
-# If you want to debug in development, comment the BasicObject line and uncomment the Object line.
-# BasicObject has no Kernel methods includes and doesn't allow pry or debug to work
-#
-Pbbuilder = Class.new(Object) # allows tools like pry
-# Pbbuilder = Class.new(BasicObject) # does not allow pry
